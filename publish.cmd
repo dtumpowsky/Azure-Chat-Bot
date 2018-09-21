9d55fc2268a32714c09f654f07d94e85f0fd8f97
David Tumpowsky <tumpowskydavid@gmail.com>
2018-09-21 12:46:42 -0700

    initial source code

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=skivacationbot-Web-Deploy.pubxml -p:Password=0iCim3wDqupH5f7GHGtKp6HL5caE5wfcstkNfdw8NyAMideGan99X8NhJijE
+
